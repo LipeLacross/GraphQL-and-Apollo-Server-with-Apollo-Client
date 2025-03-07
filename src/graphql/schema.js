e8611ea66f32fcf177f88c8ee10aa5805485837d
Felipe Moreira <feliperioscross.br@gmail.com>
2025-03-07 08:30:16 -0300

    update: context, json api, mapping types, queries, resolvers

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -1,16 +1,20 @@
 import { gql } from 'apollo-server-core';
+import { postResolvers } from './post/resolvers';
+import { postTypeDefs } from './post/typedefs';
+import { userResolvers } from './user/resolvers';
+import { userTypeDefs } from './user/typedefs';
 
 const rootTypeDefs = gql`
   type Query {
-    hi: String
+    _empty: Boolean
   }
 `;
 
 const rootResolvers = {
   Query: {
-    hi: () => 'hi again',
+    _empty: () => true,
   },
 };
 
-export const typeDefs = [rootTypeDefs];
-export const resolvers = [rootResolvers];
+export const typeDefs = [rootTypeDefs, userTypeDefs, postTypeDefs];
+export const resolvers = [rootResolvers, userResolvers, postResolvers];
